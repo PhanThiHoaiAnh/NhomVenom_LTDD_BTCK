e8b46046dc8deb2de1add8547f027bad67455788
LeThuanDieu <lethuandieu@gmail.com>
2022-12-14 23:25:20 +0700

    Dieu up lai file

---
+++ QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.quanlythuvien;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.quanlythuvien.databinding.ActivityMainBinding;
+import com.example.quanlythuvien.member.MemberFragment;
+import com.example.quanlythuvien.phieumuon.PhieuMuonFragment;
+import com.example.quanlythuvien.view.NhanVienFragment;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new NhanVienFragment());
+
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.action_home:
+                    replaceFragment(new NhanVienFragment());
+                    break;
+                case R.id.active_theloai:
+                    replaceFragment(new TheLoaiFragment());
+                    break;
+                case R.id.action_phieumuon:
+                    replaceFragment(new PhieuMuonFragment());
+                    break;
+                case R.id.action_nhanvien:
+                    replaceFragment(new NhanVienFragment());
+                    break;
+                case R.id.action_thanhvien:
+                    replaceFragment(new MemberFragment());
+                    break;
+
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+    }
---
+++ QuanLyThuVien/app/src/main/java/com/example/quanlythuvien/view/SachVanHoc.java
@@ -0,0 +1,66 @@
+package com.example.quanlythuvien.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.quanlythuvien.R;
+import com.example.quanlythuvien.phieumuon.ThemPhieuMuon;
+import com.example.quanlythuvien.sach.Sach;
+import com.example.quanlythuvien.sach.SachGiaoDucAdapter;
+import com.example.quanlythuvien.sach.SachVanHocAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class SachVanHoc extends AppCompatActivity {
+    RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    SachVanHocAdapter sachAdapter;
+    ArrayList<Sach> sachArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sach_giao_duc);
+        getSupportActionBar().hide();
+        ImageView addSach = findViewById(R.id.img_addsach);
+        addSach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ThemPhieuMuon.class);
+                startActivity(intent);
+            }
+        });
+
+        recyclerView = findViewById(R.id.rvSach);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<Sach> options =
+                new FirebaseRecyclerOptions.Builder<Sach>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("SachVanHoc"),Sach.class)
+                        .build();
+        sachAdapter = new SachVanHocAdapter(options);
+        recyclerView.setAdapter(sachAdapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sachAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sachAdapter.stopListening();
+    }
+}
